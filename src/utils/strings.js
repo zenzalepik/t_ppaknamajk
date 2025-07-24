150bbc2b752ba69ece8f59f6bf42a2ae0ba22663
Zen Zalepik <51511976+zenzalepik@users.noreply.github.com>
2025-07-24 12:12:25 +0700

    Update strings.js

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,11 +1,11 @@
 const strings = {
   appName: 'Evolusi Park',
   appDescription: 'Sistem Informasi Parkir Elektronik',
-  appLogo:'/images/png/logo.png',
+  appLogo: '/images/png/logo.png',
   developerName: 'PT. Evosist (Evolusi Sistem)',
   welcomeMessage: 'Selamat datang di sistem parkir Evosist!',
   copyRight: '© 2025 Evolusi Park – Developed by PT. Evosist (Evolusi Sistem)',
-  locationName:'Food Court Plaza Pasar Senin',
+  locationName: 'Food Court Plaza Pasar Senin',
   error: {
     notFound: 'Halaman tidak ditemukan.',
     unauthorized: 'Anda tidak memiliki izin untuk mengakses halaman ini.',
@@ -19,11 +19,12 @@ const strings = {
     //   pengaturan: 'Pengaturan',
     //   profile: 'Profil Pengguna',
   },
-  userName:'Administrator',
+  userName: 'Administrator',
   // apiUrl:'http://localhost:4000',
-  apiUrlDummy: 'http://localhost:3001',
-  apiUrl:'https://evolusipark-backend.onrender.com',
-   options: {
+  // apiUrlDummy: 'http://localhost:3001',
+  apiUrl: 'https://evolusipark-backend.onrender.com',
+  apiUrlDummy: 'https://api-dummy-evolusipark.onrender.com/',
+  options: {
     paymentTypes: ['Cash', 'Prepaid', 'Transfer Bank', 'E-Wallet', 'Member'],
   },
 };
